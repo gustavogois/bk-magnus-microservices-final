5db4410054ef196e426236d4fc73a8948f39c165
Gustavo Meneses Gois <gustavo.gois@gmail.com>
2022-12-21 22:50:29 +0000

    initial commit

---
+++ src/main/java/com/gois/bkmicroservices/BkMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.gois.bkmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BkMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gois/bkmicroservices/BkMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gois.bkmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
